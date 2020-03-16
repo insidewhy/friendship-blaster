094469f8a4964e635343b430717c7a8fd139c6e0
James Pike <jpike@chilon.net>
2020-03-16 11:35:10 +0800

    Release 1.0.0-beta.6

--- scripts/pushRelease.bash
+++ scripts/pushRelease.bash
@@ -5,4 +5,5 @@ set -e
 root="$(dirname $0)/.."
 version=$(cat $root/package.json | grep version | cut -d\" -f4)
 sed -i "s/^VERSION=.*/VERSION=$version/" $root/bin/friendship-blaster
+yarn build
 docker push xlos/friendship-blaster:$version
